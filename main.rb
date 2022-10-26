8d13582d3562e2a8ad13b1624ba120cc8a8af4d5
YasutomoNakajima <orekkueito2811@gmail.com>
2022-10-27 05:05:19 +0900

    update: 戦艦の作成にはRepositoryMockを使う

--- main.rb
+++ main.rb
@@ -1,45 +1,16 @@
 #!/bin/ruby
 
 require './Player.rb'
-require './Fleet.rb'
 require './GameMaster.rb'
 
 def main
-    cliPlayer = CLIPlayer.new("cli-player")
-    cliPlayer.addFleet(Fleet.new("cli1", 0))
-    cliPlayer.addFleet(Fleet.new("cli2", 1))
-    cliPlayer.addFleet(Fleet.new("cli3", 2))
+    cliPlayer = CLIPlayer.new('000001', "cli-player")
+    debugPlayer = DebugPlayer.new('000002', "debug-player")
 
-    debugPlayer = DebugPlayer.new("debug-player")
-    debugPlayer.addFleet(Fleet.new("dbg1", 0))
-    debugPlayer.addFleet(Fleet.new("dbg2", 1))
-    debugPlayer.addFleet(Fleet.new("dbg3", 2))
 
     gameMaster = GameMaster.new(cliPlayer, debugPlayer)
     gameMaster.game
 end
 
-def getAny
-    return 3, "aaa"
-end
-
-
-
-module Fuga
-    def hello
-        puts "hello from module"
-    end
-end
-
-class Hoge
-    include Fuga
-    def hello
-        super
-        puts "hello from method"
-    end
-end
-
-#hoge = Hoge.new
-#hoge.hello
 
 main
